256f69386830de7a1a9b5adcb7c26e8007ca049d
Lindor.Limani <Lindor.limani@gmail.com>
2026-04-02 18:51:04 +0200

    feat: First Commit + basic structure

---
+++ backend/ServiceRateBackend/src/main/java/at/hcw/serviceratebackend/ServiceRateBackendApplication.java
@@ -0,0 +1,13 @@
+package at.hcw.serviceratebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRateBackendApplication.class, args);
+	}
+
+}
---
+++ backend/ServiceRateBackend/src/test/java/at/hcw/serviceratebackend/ServiceRateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.hcw.serviceratebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
